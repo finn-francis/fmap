2751be463abbebb3d31747041c2a46dc0c3e3a46
Finn Francis <finnfrancis123@gmail.com>
2020-02-23 14:35:07 +0000

    Added basic user model to the app
    How?
    - Added default user to the seeds file.
    - Added #current_user to ApplicationController and set it to the default user in the seeds file. This means the rest of the database structure can be built so that everything ties back to a user, without having to implement devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by(email: 'finnfrancis123@gmail.com')
+  end
 end
