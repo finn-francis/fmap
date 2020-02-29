0add1abc3eab40d0687d10dad0545bd7f370bed8
Finn Francis <finnfrancis123@gmail.com>
2020-02-29 15:16:46 +0000

    Fixed parent relation for Company
    How?
    - Added the foreign_key and optional key words to allow companies to be generated with no parent.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,8 @@
 
 class Company < ApplicationRecord
   belongs_to :user
-  belongs_to :parent, class_name: 'Company', inverse_of: :children
+  belongs_to :parent, class_name: 'Company', inverse_of: :children, foreign_key: :parent_id, optional: true
+
   has_many :children, class_name: 'Company', inverse_of: :parent, dependent: :restrict_with_error
 
   validates :user_id, presence: true
