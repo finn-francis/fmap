15fd5533253c9765da679c426d3c861ddaa59f1a
Finn Francis <finnfrancis123@gmail.com>
2020-02-29 13:49:35 +0000

    Added specs for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,9 +2,9 @@
 
 class Company < ApplicationRecord
   belongs_to :user
-
   belongs_to :parent, class_name: 'Company', inverse_of: :children
   has_many :children, class_name: 'Company', inverse_of: :parent, dependent: :restrict_with_error
 
   validates :user_id, presence: true
+  validates :name, presence: true
 end
---
+++ spec/models/company_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  describe 'validations' do
+    it { is_expected.to(validate_presence_of(:user_id)) }
+    it { is_expected.to(validate_presence_of(:name)) }
+  end
+
+  describe 'relations' do
+    it { is_expected.to(have_many(:children)) }
+    it { is_expected.to(belong_to(:parent)) }
+  end
+end
